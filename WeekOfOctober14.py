8dbebcb0ac543d24dd83cee36cd1d98f7eab76ba
IzzieFedynich <54276173+IzzieFedynich@users.noreply.github.com>
2019-10-18 09:03:04 -0500

    Update WeekOfOctober14.py

--- WeekOfOctober14.py
+++ WeekOfOctober14.py
@@ -105,7 +105,7 @@ print(dogWeight <= 25)
 # equals is an assignment equal to is to compare
 catWeight = 15
 
-# compoud Boolean operatiors
+# compound Boolean operators
 # snf
 # or
 # not-false
@@ -136,4 +136,22 @@ else:
         c = 25
 
 
-print(a,b,c)
+print(a, b, c)
+
+
+d = 85
+e = 72
+f = 44
+ans = 0
+
+if d > e:
+    ans= 12
+else:
+    if d == e:
+        ans = 50
+    else:
+        if f < d * e:
+            ans = 100
+        else:
+            ans = 75
+print(ans)
